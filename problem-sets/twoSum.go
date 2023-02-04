7770e12de21de7686dc19dabbda4aae0d8178c50
amirreza <amir.rezakheradmand.2017@gmail.com>
2023-02-04 14:37:54 +0330

    add: add two sum

--- problem-sets/twoSum.go
+++ problem-sets/twoSum.go
@@ -1,1 +1,25 @@
-package problem_sets
+package main
+
+import "fmt"
+
+func twoSum(nums []int, target int) []int {
+	var result []int
+
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				result = append(result, i)
+				result = append(result, j)
+			}
+		}
+	}
+
+	return result
+}
+
+func main() {
+	nums := []int{2, 7, 11, 15}
+	target := 9
+	s := twoSum(nums, target)
+	fmt.Println(s)
+}
