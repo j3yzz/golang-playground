e506c3c1bfb56368e605879474382c1b3ffd3faa
amirreza <amir.rezakheradmand.2017@gmail.com>
2023-02-25 13:53:50 +0330

    add some code for context package

---
+++ channel/context/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"runtime"
+	"time"
+)
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	fmt.Println("error check 1:", ctx.Err())
+	fmt.Println("num goroutines 1:", runtime.NumGoroutine())
+
+	go func() {
+		n := 0
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				n++
+				time.Sleep(time.Millisecond * 200)
+				fmt.Println("working", n)
+			}
+		}
+	}()
+
+	time.Sleep(time.Second * 2)
+	fmt.Println("error check 2:", ctx.Err())
+	fmt.Println("num goroutines 2:", runtime.NumGoroutine())
+
+	fmt.Println("about to cancel context")
+	cancel()
+	fmt.Println("cancelled context")
+	time.Sleep(time.Second * 2)
+	fmt.Println("error check 3:", ctx.Err())
+	fmt.Println("num goroutines 3:", runtime.NumGoroutine())
+}
