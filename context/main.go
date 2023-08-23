632735c9518d56b126e7ec97982ce771fc40b7fb
Amirreza Kheradmand <amir.rezakheradmand.2017@gmail.com>
2023-08-23 23:23:55 +0330

    add: add context with cancel

--- context/main.go
+++ context/main.go
@@ -32,6 +32,29 @@ func manager(ctx context.Context) {
 }
 
 func main() {
+	ctx := context.Background()
+	cancelCtx, cancelFunc := context.WithCancel(ctx)
+	go task(cancelCtx)
+	time.Sleep(time.Second * 3)
+	cancelFunc()
+	time.Sleep(time.Second * 1)
+
 	http.HandleFunc("/hello", hello)
 	http.ListenAndServe(":8080", nil)
+}
+
+func task(ctx context.Context) {
+	i := 1
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("Gracefully exit")
+			fmt.Println(ctx.Err())
+			return
+		default:
+			fmt.Println(i)
+			time.Sleep(time.Second * 1)
+			i++
+		}
+	}
 }
