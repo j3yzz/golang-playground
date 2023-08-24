f7ec57c0001a81c750afa061ed464210901c548c
Amirreza Kheradmand <amir.rezakheradmand.2017@gmail.com>
2023-08-24 13:38:47 +0330

    add: add context with timeout

--- context/main.go
+++ context/main.go
@@ -3,54 +3,76 @@ package main
 import (
 	"context"
 	"fmt"
-	"math/rand"
-	"net/http"
 	"time"
 )
 
-func hello(w http.ResponseWriter, req *http.Request) {
-	ctx := req.Context()
-
-	childCtx := context.WithValue(ctx, "rand_token", rand.Uint32())
-	manager(childCtx)
-
-	fmt.Println("server: hello handler started")
-	defer fmt.Println("server: hello handler ended")
-
-	select {
-	case <-time.After(10 * time.Second):
-		fmt.Fprintf(w, "hello\n")
-	case <-ctx.Done():
-		err := ctx.Err()
-		fmt.Println("server:", err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-}
-
-func manager(ctx context.Context) {
-	fmt.Println(ctx.Value("rand_token"))
-}
+//
+//func hello(w http.ResponseWriter, req *http.Request) {
+//	ctx := req.Context()
+//
+//	childCtx := context.WithValue(ctx, "rand_token", rand.Uint32())
+//	manager(childCtx)
+//
+//	fmt.Println("server: hello handler started")
+//	defer fmt.Println("server: hello handler ended")
+//
+//	select {
+//	case <-time.After(10 * time.Second):
+//		fmt.Fprintf(w, "hello\n")
+//	case <-ctx.Done():
+//		err := ctx.Err()
+//		fmt.Println("server:", err)
+//		http.Error(w, err.Error(), http.StatusInternalServerError)
+//	}
+//}
+//
+//func manager(ctx context.Context) {
+//	fmt.Println(ctx.Value("rand_token"))
+//}
+//
+//func main() {
+//	ctx := context.Background()
+//	cancelCtx, cancelFunc := context.WithCancel(ctx)
+//	go task(cancelCtx)
+//	time.Sleep(time.Second * 3)
+//	cancelFunc()
+//	time.Sleep(time.Second * 1)
+//
+//	http.HandleFunc("/hello", hello)
+//	http.ListenAndServe(":8080", nil)
+//}
+//
+//func task(ctx context.Context) {
+//	i := 1
+//	for {
+//		select {
+//		case <-ctx.Done():
+//			fmt.Println("Gracefully exit")
+//			fmt.Println(ctx.Err())
+//			return
+//		default:
+//			fmt.Println(i)
+//			time.Sleep(time.Second * 1)
+//			i++
+//		}
+//	}
+//}
 
 func main() {
 	ctx := context.Background()
-	cancelCtx, cancelFunc := context.WithCancel(ctx)
-	go task(cancelCtx)
-	time.Sleep(time.Second * 3)
-	cancelFunc()
-	time.Sleep(time.Second * 1)
-
-	http.HandleFunc("/hello", hello)
-	http.ListenAndServe(":8080", nil)
+	cancelCtx, cancelFunc := context.WithTimeout(ctx, time.Second*3)
+	defer cancelFunc()
+	go task1(cancelCtx)
+	time.Sleep(time.Second * 4)
 }
 
-func task(ctx context.Context) {
+func task1(ctx context.Context) {
 	i := 1
 	for {
 		select {
 		case <-ctx.Done():
 			fmt.Println("Gracefully exit")
 			fmt.Println(ctx.Err())
-			return
 		default:
 			fmt.Println(i)
 			time.Sleep(time.Second * 1)
